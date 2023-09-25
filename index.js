e37096446986611632e4e23e1fe686c7f3be38f5
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-24 20:45:07 -0400

    proof of concept for a cli app

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+#!/usr/bin/env node
+
 // console.log(global);
 // console.log(process.argv);
 // console.log(process.env);
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
+
+const note = process.argv.slice(2);
+const newNote = {
+  content: note,
+  id: Date.now(),
+};
+
+console.log(newNote);
