dab979c093faf8e333d73048a8ceede71bfcea43
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-09-25 17:57:04 -0400

    setting up yargs

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 #!/usr/bin/env node
-import fs from "node:fs";
-
-// const note = process.argv.slice(2);
-// const newNote = {
-//   content: note,
-//   id: Date.now(),
-// };
-
-// console.log(newNote);
+import "./src/command.js";
